3041ac2610378d9d15a0fd36c95898ecd2e2f29d
getSomeCats <crealxw@gmail.com>
2026-05-30 12:23:13 +0800

    Refactor: Remove daed package integration and related configurations from build scripts

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -5,11 +5,9 @@ patch -p1 -f < $(dirname "$0")/luci.patch
 
 # Clone packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
-git clone https://github.com/QiuSimons/luci-app-daed clone/daed
 
 # Adjust packages
-rm -rf feeds/luci/applications/luci-app-daed
-cp -rf clone/amlogic/luci-app-amlogic clone/daed/luci-app-daed feeds/luci/applications/
+cp -rf clone/amlogic/luci-app-amlogic feeds/luci/applications/
 sed -i '/luci-app-attendedsysupgrade/d' feeds/luci/collections/luci/Makefile
 
 # Clean packages
